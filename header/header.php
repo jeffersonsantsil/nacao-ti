c4563fda911e17508ef6299c020cfc8e92cbdb6e
Jefferson dos Santos Silva <jeffersonsantsil@gmail.com>
2021-07-06 00:07:19 -0300

    Add files via upload

---
+++ header/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nação TI</title>
+
+    <!--DECLARANDO O BOOTSTRAP 4-->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <!--DECLARANDO O MEU STYLE-->
+    <link rel="stylesheet" href="public/css/style.css">
+
+</head>
+
+<header>
+    <div class="container" id="nav-container">
+        <nav class="navbar navbar-expand-lg">
+            <a href="http://nacaoti.com.br" class="navbar-brand">
+                <img id="logo" src="public/imgs/logo_nacaoti.svg" alt="logo nacao ti"></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!--LINKS DO MENU-->
+            <div class="collapse navbar-collapse justify-content-end" id="menu">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quem Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Equipe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+</header>
+
+<body>
