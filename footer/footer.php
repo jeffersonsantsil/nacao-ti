a61d0c1aeff83a58a84a8441435a128f46455419
Jefferson dos Santos Silva <jeffersonsantsil@gmail.com>
2021-07-06 00:07:43 -0300

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,9 @@
+</body>
+    <footer>
+    <!--DECLARANDO JS-->
+    <script src="public/js/jquery.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+    
+    </footer>
+
+</html>
